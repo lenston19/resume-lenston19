3ff6234ec4dd00f4fe5a4ae749ac8d7f1801ed0d
lenston19 <dimon.smolonogov@gmail.com>
2022-05-20 16:12:03 +0300

    added stars on background

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,9 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container style="padding-top: 0; background: #000">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
       <router-view />
     </q-page-container>
   </q-layout>
