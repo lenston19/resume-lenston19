fbdd7c37d53862191cbbf0d480f3a06ae7b846d5
lenston19 <lenston190900@gmail.com>
2022-06-29 10:49:05 +0300

    added intersection

--- src/components/ResumeBeginningText.vue
+++ src/components/ResumeBeginningText.vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="container beginning-text">
+  <div class="container mh-100 beginning-text">
     <div class="row justify-center">
-      <div class="col-12">
-        <div class="beginning-text__caption">Episode VI</div>
-        <div class="beginning-text__title text-weigth-bolder q-my-md">
-          Return of the Jedi
+      <q-intersection
+        once
+        transition-duration="1500"
+        transition="fade"
+        class="beginning-text__item"
+      >
+        <div class="col-12">
+          <div class="beginning-text__caption">Episode VI</div>
+          <div class="beginning-text__title text-weigth-bolder q-my-md">
+            Return of the Jedi
+          </div>
         </div>
-      </div>
-      <div class="col-6">
-        <div class="text-h6 beginning-text__description">
-          Turmoil has engulfed the Galactic Republic. The taxation of trade
-          routes to outlying star systems is in dispute. Hoping to resolve the
-          matter with a blockade of deadly battleships, the greedy Trade
-          Federation has stopped all shipping to the small planet of Naboo.
-          While the congress of the Republic endlessly debates this alarming
-          chain of events, the Supreme Chancellor has secretly dispatched two
-          Jedi Knights, the guardians of peace and justice in the galaxy, to
-          settle the conflict....
+        <q-intersection
+          once
+          transition-duration="2000"
+          transition="fade"
+          class="beginning-text__item"
+        >
+        </q-intersection>
+        <div class="col-6">
+          <div class="text-h6 beginning-text__description">
+            Turmoil has engulfed the Galactic Republic. The taxation of trade
+            routes to outlying star systems is in dispute. Hoping to resolve the
+            matter with a blockade of deadly battleships, the greedy Trade
+            Federation has stopped all shipping to the small planet of Naboo.
+            While the congress of the Republic endlessly debates this alarming
+            chain of events, the Supreme Chancellor has secretly dispatched two
+            Jedi Knights, the guardians of peace and justice in the galaxy, to
+            settle the conflict....
+          </div>
         </div>
-      </div>
+      </q-intersection>
     </div>
   </div>
 </template>
--- src/components/ResumeIntro.vue
+++ src/components/ResumeIntro.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="container intro">
+  <div class="container intro mh-100">
     <div class="row justify-center">
-      <div class="col-12">
-        <div class="text-h5 intro__title">
-          A long time ago in a galaxy far, <br />far away....
+      <q-intersection
+        once
+        transition-duration="1500"
+        transition="fade"
+        class="intro__item"
+      >
+        <div class="col-12">
+          <div class="text-h5 intro__title">
+            A long time ago in a galaxy far, <br />far away....
+          </div>
         </div>
-      </div>
+      </q-intersection>
     </div>
+
+    <q-icon
+      class="intro__next-step"
+      name="las la-sort-down"
+      color="white"
+      size="2rem"
+    />
   </div>
 </template>
 
